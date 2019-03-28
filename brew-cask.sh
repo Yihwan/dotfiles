b8482dcc9e319513272c4690c77e6b01221eebc3
yihwan <hi@yihwan.kim>
2019-03-28 09:31:39 -0700

    add brew and brew-cask files

---
+++ brew-cask.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+brew cask install firefox
+brew cask install 1password
+brew cask install atom
+brew cask install iterm2
+brew cask install sequel-pro
+brew cask install slack
+brew cask install zoomus
+brew cask install docker
+brew cask install protonvpn
+
+brew cask install spotify
